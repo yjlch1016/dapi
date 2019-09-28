6ece078274b29990ad918b6184b48e7158c3d032
yangjianliang <526861348@qq.com>
2019-09-29 00:05:24 +0800

    修改运行用例组视图

--- interface/views.py
+++ interface/views.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 import demjson
 import requests
 from django.contrib import auth
@@ -320,7 +322,81 @@ class DeleteCaseGroupView(LoginRequiredMixin, View):
 class DebugCaseGroupView(LoginRequiredMixin, View):
     """运行用例组"""
 
+    def update_interface_info(self, case_id, field, value):
+        # 等价于UPDATE interface_info SET field=value WHERE id=case_id;
+        field_value = {field: value}
+        InterfaceInfo.objects.filter(id=case_id).update(**field_value)
+
     def post(self, request):
+        case_group_id = request.POST.get('form_case_group_id_b', '')
+        data_object = CaseGroupInfo.objects.get(
+            id=case_group_id).groups.values(
+            "id", "request_mode", "interface_url",
+            "body_type", "request_body", "request_head",
+            "request_parameter", "expected_result", "response_assert"
+        ).order_by("id")
+        # 反向查询用例组包含的用例
+
+        data_list = list(data_object)
+        # 把QuerySet对象转换成列表
+        for item in data_list:
+            case_id = item["id"]
+            request_mode = item["request_mode"]
+            interface_url = item["interface_url"]
+            body_type = item["body_type"]
+            request_body = item["request_body"]
+            request_head = item["request_head"]
+            request_parameter = item["request_parameter"]
+            expected_result = item["expected_result"]
+            response_assert = item["response_assert"]
+            # 获取列表里面的字典的值
+
+            if body_type == "x-www-form-urlencoded":
+                pass
+            elif body_type == "json":
+                request_body = demjson.decode(request_body)
+                # 等价于json.loads()
+
+            response = requests.request(
+                request_mode,
+                interface_url,
+                data=request_body,
+                headers=demjson.decode(request_head),
+                params=demjson.decode(request_parameter)
+            )
+
+            result_code = response.status_code
+            # 实际的响应代码
+            print(result_code)
+            result_text = response.text
+            # 实际的响应文本
+            print(result_text)
+            expect_error = "接口请求失败，请检查是否拼写错误！"
+
+            if result_code == 200:
+                if response_assert == "包含":
+                    self.update_interface_info(case_id, "response_code", result_code)
+                    # 插入响应代码
+                    self.update_interface_info(case_id, "actual_result", result_text)
+                    # 插入实际结果
+                    if expected_result in result_text:
+                        self.update_interface_info(case_id, "pass_status", 1)
+                        # 插入通过状态
+                    else:
+                        self.update_interface_info(case_id, "pass_status", 0)
+                        # 插入不通过状态
+                elif response_assert == "相等":
+                    self.update_interface_info(case_id, "response_code", result_code)
+                    self.update_interface_info(case_id, "actual_result", result_text)
+                    if expected_result == result_text:
+                        self.update_interface_info(case_id, "pass_status", 1)
+                    else:
+                        self.update_interface_info(case_id, "pass_status", 0)
+            else:
+                self.update_interface_info(case_id, "response_code", result_code)
+                self.update_interface_info(case_id, "actual_result", expect_error)
+                self.update_interface_info(case_id, "pass_status", 0)
+
         return redirect('/case_group/')
 
 
@@ -463,7 +539,7 @@ class DebugInterfaceView(LoginRequiredMixin, View):
     """运行用例"""
 
     def update_interface_info(self, case_id, field, value):
-        # UPDATE interface_info SET field=value WHERE id=table_id;
+        # 等价于UPDATE interface_info SET field=value WHERE id=case_id;
         field_value = {field: value}
         InterfaceInfo.objects.filter(id=case_id).update(**field_value)
 
