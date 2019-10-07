aec63fb40430eaee47b4cfc65989f9615952ff9f
yangjianliang <526861348@qq.com>
2019-10-07 20:31:03 +0800

    增加celery

--- dapi/__init__.py
+++ dapi/__init__.py
@@ -1,4 +1,8 @@
+from __future__ import absolute_import, unicode_literals
+from dapi.celery import app as celery_app
 import pymysql
+
+__all__ = ('celery_app',)
 
 pymysql.install_as_MySQLdb()
 # PyMySQL初始化
